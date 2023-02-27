3be9b90d960a401d4158ac62306e633888911946
Samsonasumu <samsonasumu@gmail.com>
2023-02-28 02:35:37 +0300

    updated the repository

---
+++ bookstore/books/admin.py
@@ -0,0 +1,8 @@
+ 
+# Register your models here.
+from django.contrib import admin
+# from the models.py file import Book
+from .models import Book
+
+# registering the Book to the admin site
+admin.site.register(Book)
